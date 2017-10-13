82225b11aeb44511b09ae769a8460518a7b44aa8
Mikhail <32748867+bazarinm@users.noreply.github.com>
2017-10-13 14:43:48 +0300

    Update Main.cpp

--- Library/Main.cpp
+++ Library/Main.cpp
@@ -7,14 +7,11 @@ void main()
 	Library my_library;
 	Console my_console(my_library);
 	int operation;
-
-	//my_library.Rename("Biblioteka im. Lenina");
-	//my_library.AddBook("Voina i Mir T1", "L.N. Tolstoy", 1876);
-	//my_library.AddBook("Voina i Mir T2", "L.N. Tolstoy", 1878);
-	//my_library.AddBook("Prestuplenie i Nakazanie", "F.M. Dostoevsky", 1876);
-	//my_library.AddBook("Nos", "A.S. Pushkin", 1778);
-	//my_library.RemoveBook("Nos", "A.S. Pushkin");
-	//my_library.RemoveAuthor("L.N. Tolstoy");
+	
+	my_library.Rename("Biblioteka im. Lenina");
+	my_library.AddBook("Voina i Mir T1", "L.N. Tolstoy", 1876);
+	my_library.AddBook("Voina i Mir T2", "L.N. Tolstoy", 1878);
+	my_library.AddBook("Prestuplenie i Nakazanie", "F.M. Dostoevsky", 1876);
 
 	do {
 		std::cout << std::endl << "Enter 1 to add author, 2 to add book," << std::endl;
